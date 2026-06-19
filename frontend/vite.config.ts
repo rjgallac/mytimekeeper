198db353926611772f81eefe017c0782f339eec4
rjgallac <rjgallac@hotmail.com>
2026-06-19 22:45:03 +0100

    update api url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,19 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:3001'
-    }
+      "/api": "https://timekeeperbackend.sheffieldwebprogrammer.co.uk`",
+    },
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-})
+});
